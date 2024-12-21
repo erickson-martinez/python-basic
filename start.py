75b80f3d31ae4a6d2d9a45d270c42960eb56c747
erickson-martinez <erickson.martinez.pro@gmail.com>
2024-12-20 22:27:04 -0300

    feat: numbers class

--- start.py
+++ start.py
@@ -12,3 +12,13 @@ print('String')
 print("String")
 print(str(numero))
 print(nome)
+
+# numbers
+number_int = 1
+number_float = 3.14
+number_int_string = '1'
+number_float_string = '3.14'
+print(number_int)
+print(number_float)
+print(int(number_int_string))
+print(float(number_float_string))
