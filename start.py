3c7910b636bb43df1750c0ea03050bcfbb86452d
erickson-martinez <erickson.martinez.pro@gmail.com>
2024-12-20 22:17:51 -0300

    feat: string class

--- start.py
+++ start.py
@@ -3,4 +3,12 @@ print("Hello World")
 #Variavel
 variavel_name = "Erickson"
 variavel_last = "Martinez"
-print(variavel_name+ ""+variavel_last)
+print(variavel_name+ " "+variavel_last)
+
+# String
+nome = "string"
+numero = 1
+print('String')
+print("String")
+print(str(numero))
+print(nome)
